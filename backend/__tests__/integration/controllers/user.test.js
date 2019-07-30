b1a4ba3b1f69cb0436c7144fde8d09ed7ce0cc02
Rafael Sene <rafaelssene@gmail.com>
2019-07-30 19:34:55 -0300

    implementing user update, auth and all tests.

--- backend/__tests__/integration/controllers/user.test.js
+++ backend/__tests__/integration/controllers/user.test.js
@@ -53,4 +53,142 @@ describe('User', () => {
       expect(user.status).toBe(400);
     });
   });
+
+  describe('Update', () => {
+    beforeEach(async () => {
+      await truncate();
+    });
+
+    it('should be able to rename', async () => {
+      const user = await factory.attrs('User');
+
+      await request(app)
+        .post('/users')
+        .send(user);
+
+      const login = await request(app)
+        .post('/sessions')
+        .send({ email: user.email, password: user.password });
+
+      const response = await request(app)
+        .put('/users')
+        .set('Authorization', `Bearer ${login.body.token}`)
+        .send({ name: 'Teste name', email: user.email });
+
+      expect(response.status).toBe(200);
+    });
+
+    it('it is not possible to pass password and blank email', async () => {
+      const user = await factory.attrs('User');
+
+      await request(app)
+        .post('/users')
+        .send(user);
+
+      const login = await request(app)
+        .post('/sessions')
+        .send({ email: user.email, password: user.password });
+
+      const response = await request(app)
+        .put('/users')
+        .set('Authorization', `Bearer ${login.body.token}`)
+        .send({});
+
+      expect(response.status).toBe(400);
+    });
+
+    it('checking if email already exists', async () => {
+      const user = {
+        name: 'Novo Teste',
+        email: 'teste@gmail.com',
+        password: '123456',
+      };
+      const userTwo = await factory.attrs('User');
+
+      await request(app)
+        .post('/users')
+        .send(user);
+
+      await request(app)
+        .post('/users')
+        .send(userTwo);
+
+      const login = await request(app)
+        .post('/sessions')
+        .send({ email: user.email, password: user.password });
+
+      const response = await request(app)
+        .put('/users')
+        .set('Authorization', `Bearer ${login.body.token}`)
+        .send({ name: user.name, email: userTwo.email });
+
+      expect(response.status).toBe(400);
+    });
+
+    it('checking old password is wrong', async () => {
+      const user = await factory.attrs('User');
+
+      await request(app)
+        .post('/users')
+        .send(user);
+
+      const login = await request(app)
+        .post('/sessions')
+        .send({ email: user.email, password: user.password });
+
+      const response = await request(app)
+        .put('/users')
+        .set('Authorization', `Bearer ${login.body.token}`)
+        .send({
+          name: user.name,
+          email: user.email,
+          oldPassword: '111111',
+          password: '000000',
+          confirmPassword: '000000',
+        });
+
+      expect(response.status).toBe(400);
+    });
+
+    it('trying to update without authentication', async () => {
+      const user = await factory.attrs('User');
+
+      await request(app)
+        .post('/users')
+        .send(user);
+
+      const response = await request(app)
+        .put('/users')
+        .send({
+          name: user.name,
+          email: user.email,
+          oldPassword: '111111',
+          password: '000000',
+          confirmPassword: '000000',
+        });
+
+      expect(response.status).toBe(401);
+    });
+
+    it('passing invalid token', async () => {
+      const user = await factory.attrs('User');
+
+      await request(app)
+        .post('/users')
+        .send(user);
+
+      const response = await request(app)
+        .put('/users')
+        .set('Authorization', `Bearer tokenInvalido`)
+        .send({
+          name: user.name,
+          email: user.email,
+          oldPassword: '111111',
+          password: '000000',
+          confirmPassword: '000000',
+        });
+
+      expect(response.status).toBe(401);
+    });
+  });
 });
--- backend/src/app/controllers/UserController.js
+++ backend/src/app/controllers/UserController.js
@@ -27,6 +27,49 @@ class UserController {
 
     return res.json({ id, name, email });
   }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+    const { id, email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    if (email !== user.email) {
+      const userExists = await User.findOne({
+        where: { email },
+      });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'User already exists.' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(400).json({ error: 'Password does not match' });
+    }
+
+    const { name, email: newEmail } = await user.update(req.body);
+
+    return res.json({ id, name, newEmail });
+  }
 }
 
 export default new UserController();
